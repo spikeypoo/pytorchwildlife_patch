0f4c3886036f732b8d6f7cd4ac016e1897996c3c
spikeypoo <>
2025-09-06 17:26:17 -0400

    Revert "Update setup.py"
    This reverts commit a0c844ddfbda224489b8867aa2af53e40e419811.

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup, find_packages
 
 with open('README.md', encoding="utf8") as file:
-    long_description = file.read()
-
+        long_description = file.read()
 setup(
     name='PytorchWildlife',
-    version='1.2.4.1',
+    version='1.2.4.1', 
     packages=find_packages(),
-    url='https://github.com/microsoft/CameraTraps/',
+    url='https://github.com/microsoft/CameraTraps/',  
     license='MIT',
     author='Andres Hernandez, Zhongqi Miao, Daniela Ruiz Lopez, Isai Daniel Chacon Silva',
-    author_email='v-hernandres@microsoft.com, zhongqimiao@microsoft.com, v-druizlopez@microsoft.com, v-ichaconsil@microsoft.com',
+    author_email='v-hernandres@microsoft.com, zhongqimiao@microsoft.com, v-druizlopez@microsoft.com, v-ichaconsil@microsoft.com',  
     description='a PyTorch Collaborative Deep Learning Framework for Conservation.',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -19,7 +18,7 @@ setup(
         'torchvision',
         'torchaudio',
         'tqdm',
-        'Pillow',
+        'Pillow', 
         'supervision==0.23.0',
         'gradio',
         'ultralytics',
@@ -30,13 +29,9 @@ setup(
         'scikit-learn',
         'timm',
     ],
-    include_package_data=True,
-    package_data={
-        "": ["*.yml"],
-    },
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',  
+        'Intended Audience :: Developers', 
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
     ],
